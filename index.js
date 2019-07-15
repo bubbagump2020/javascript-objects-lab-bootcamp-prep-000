28886a7dfca45a25190bd2ffb169a1160d571dac
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-15 17:06:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  return Object.assign{}, {value}
+  return Object.assign{}, {key}
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
